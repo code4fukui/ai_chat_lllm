01306a8bdff710a44d54d3206c4330b61468849b
Taisuke Fukuno <fukuno@jig.jp>
2024-07-11 08:18:28 +0900

    add log

--- aichat.js
+++ aichat.js
@@ -1,11 +1,14 @@
 import { serveAPI } from "https://js.sabae.cc/wsutil.js";
 //import { fetchReply } from "./fetchReply.js";
 import { fetchReplyWithCache } from "./fetchReplyWithCache.js";
+import { log } from "./log.js";
 
 serveAPI("/api", async (param, req, path) => {
   if (path == "/api/reply") {
     //return await fetchReply(param);
-    return await fetchReplyWithCache(param);
+    const res = await fetchReplyWithCache(param);;
+    await log({ q: param, res });
+    return res;
   }
   return null;
 });
